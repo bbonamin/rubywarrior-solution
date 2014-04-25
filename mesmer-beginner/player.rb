d9a9b25e418cdb406fe7d1ef161b79e3382ab60f
Bruno Bonamin <bruno@bonamin.org>
2014-04-25 09:44:01 -0300

    Level 3 done

--- mesmer-beginner/player.rb
+++ mesmer-beginner/player.rb
@@ -1,9 +1,20 @@
 class Player
   def play_turn(warrior)
-    if warrior.feel.empty?
-      warrior.walk!
-    else
-      warrior.attack!
+    catch :turn_done do
+      ensure_healthy(warrior)
+
+      if warrior.feel.empty?
+        warrior.walk!
+      else
+        warrior.attack!
+      end
     end
   end
+
+  def ensure_healthy(warrior)
+    if warrior.health < 10 and warrior.feel.empty?
+      warrior.rest!
+      throw :turn_done
+    end
+  end 
 end
