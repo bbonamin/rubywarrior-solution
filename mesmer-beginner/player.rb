a1ca7d474c32f8e8059f5c2fa373ba5f5962e7bf
Bruno Bonamin <bruno@bonamin.org>
2014-04-25 10:32:19 -0300

    Level 5 complete
    Incremental refactor out with a decorator
    
    Health storage working by extracting to separate object
    
    Introduces smart attack to rescue captives

--- mesmer-beginner/player.rb
+++ mesmer-beginner/player.rb
@@ -1,46 +1,62 @@
+require 'delegate'
 class Player
   def play_turn(warrior)
-    #@barbarian ||= Barbarian.from_warrior(warrior)
-    
+    @health ||= Health.new
+    @barbarian = Barbarian.from_warrior(warrior)
+    @barbarian.health_collaborator = @health
+ 
     catch :turn_done do
-      if warrior.feel.empty? 
-        ensure_healthy(warrior)
-        warrior.walk!
+      if @barbarian.feel.empty? 
+        @barbarian.ensure_healthy
+        @barbarian.walk!
       else
-        warrior.attack!
+        @barbarian.smart_attack!
       end
     end
-    @previous_health = warrior.health
+    @health.previous = @barbarian.health
   end
 
-  def ensure_healthy(warrior)
-    if health_below_safe_threshold?(warrior) and warrior.feel.empty? and not(taking_damage?(warrior))
-      warrior.rest!
-      throw :turn_done
-    end
-  end 
- 
-  def health_below_safe_threshold?(warrior)
-    warrior.health < 15
-  end
- 
-  def taking_damage?(warrior)
-    @previous_health ||= 0
-    warrior.health < @previous_health
-  end
-  
-  def retreat(warrior)
+    def retreat(warrior)
     warrior.walk! :backward
   end
 end
 
 # Enhanced Warrior
-class Barbarian
+class Barbarian < SimpleDelegator
+  attr_accessor :health_collaborator
+
   def self.from_warrior(warrior)
-    new(warrior: warrior)
+    new(warrior)
   end
   
-  def initialize(options = {})
-    @warrior = options[:warrior]
+  def ensure_healthy
+    if health_below_safe_threshold? and feel.empty? and not(taking_damage?)
+      rest!
+      throw :turn_done
+    end
   end
+  
+  def health_below_safe_threshold?
+    health < 15
+  end
+ 
+  def taking_damage?
+    if health < health_collaborator.previous
+      true
+    else
+      puts 'Not taking damage'
+    end
+  end
+
+  def smart_attack!
+    if feel.captive?
+      rescue!
+    else
+      attack!
+    end
+  end 
+end
+
+class Health
+  attr_accessor :previous
 end
