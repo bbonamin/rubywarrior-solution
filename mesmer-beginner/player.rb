a026f6be857853afffc3340eedb491ccad9fe433
Bruno Bonamin <bruno@bonamin.org>
2014-04-25 10:15:49 -0300

    Level 4 complete

--- mesmer-beginner/player.rb
+++ mesmer-beginner/player.rb
@@ -1,20 +1,46 @@
 class Player
   def play_turn(warrior)
+    #@barbarian ||= Barbarian.from_warrior(warrior)
+    
     catch :turn_done do
-      ensure_healthy(warrior)
-
-      if warrior.feel.empty?
+      if warrior.feel.empty? 
+        ensure_healthy(warrior)
         warrior.walk!
       else
         warrior.attack!
       end
     end
+    @previous_health = warrior.health
   end
 
   def ensure_healthy(warrior)
-    if warrior.health < 10 and warrior.feel.empty?
+    if health_below_safe_threshold?(warrior) and warrior.feel.empty? and not(taking_damage?(warrior))
       warrior.rest!
       throw :turn_done
     end
   end 
+ 
+  def health_below_safe_threshold?(warrior)
+    warrior.health < 15
+  end
+ 
+  def taking_damage?(warrior)
+    @previous_health ||= 0
+    warrior.health < @previous_health
+  end
+  
+  def retreat(warrior)
+    warrior.walk! :backward
+  end
+end
+
+# Enhanced Warrior
+class Barbarian
+  def self.from_warrior(warrior)
+    new(warrior: warrior)
+  end
+  
+  def initialize(options = {})
+    @warrior = options[:warrior]
+  end
 end
